d239cbd4919d41ea478b29dfd906fbffb429b935
Daniel Bednorz <daniel.bednorz@novatec-gmbh.de>
2017-01-10 08:43:52 +0100

    add webtester2-tests

---
+++ src/main/java/dbe/pageobject/google/Result.java
@@ -0,0 +1,18 @@
+package dbe.pageobject.google;
+
+import info.novatec.testit.webtester.conditions.pagefragments.Visible;
+import info.novatec.testit.webtester.pagefragments.Span;
+import info.novatec.testit.webtester.pagefragments.annotations.IdentifyUsing;
+import info.novatec.testit.webtester.pagefragments.annotations.PostConstructMustBe;
+import info.novatec.testit.webtester.pagefragments.annotations.WaitUntil;
+import info.novatec.testit.webtester.pagefragments.identification.producers.Id;
+import info.novatec.testit.webtester.pages.Page;
+
+public interface Result extends Page{
+
+	@WaitUntil(Visible.class)
+	@PostConstructMustBe(Visible.class)
+	@IdentifyUsing(value="cwos", how=Id.class)
+	Span result();
+	
+}
